815d7b6312731bf3e387c42f8e889f122da6133e
Dirk Sierd de Vries <dirksierd@me.com>
2014-01-15 18:05:41 +0100

    Removed warning

--- lib/cucumber/rails.rb
+++ lib/cucumber/rails.rb
@@ -14,9 +14,9 @@ if env_caller
     require 'action_dispatch/testing/integration'
   end
 
-  if !Rails.application.config.cache_classes
-    warn "WARNING: You have set Rails' config.cache_classes to false (most likely in config/environments/cucumber.rb).  This setting is known to cause problems with database transactions. Set config.cache_classes to true if you want to use transactions.  For more information see https://rspec.lighthouseapp.com/projects/16211/tickets/165."
-  end
+  #if !Rails.application.config.cache_classes
+  #  warn "WARNING: You have set Rails' config.cache_classes to false (most likely in config/environments/cucumber.rb).  This setting is known to cause problems with database transactions. Set config.cache_classes to true if you want to use transactions.  For more information see https://rspec.lighthouseapp.com/projects/16211/tickets/165."
+  #end
 
   require 'cucumber/rails/world'
   require 'cucumber/rails/hooks'
